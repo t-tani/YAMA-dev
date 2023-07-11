6d4f304625f2f04f1b42aee6ee6369f7fbecccf0
t-tani <31282248+t-tani@users.noreply.github.com>
2023-07-11 23:10:20 +0900

    fix

--- yara-update-build.bat
+++ yara-update-build.bat
@@ -2,9 +2,15 @@
 
 python utils\arc4.py -f .\rsrc\ioc\rule.yara
 
-@REM update resource object
-
+@REM update project
 cd build
+cmake -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang.exe" ^
+      -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang++.exe" ^
+      -DCMAKE_RC_COMPILER="C:/Program Files/LLVM/bin/llvm-rc.exe" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -G Ninja ..
+
+@REM update resource object
 ninja -t clean CMakeFiles/Yama.dir/rsrc/resources.rc.res > yama-ninja.log
 ninja -v CMakeFiles/Yama.dir/rsrc/resources.rc.res >> yama-ninja.log
 
