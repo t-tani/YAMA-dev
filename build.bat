1085dbf1969048949d0060576c5125900c6d1261
t-tani <31282248+t-tani@users.noreply.github.com>
2023-07-11 21:17:04 +0900

    fix

---
+++ build.bat
@@ -0,0 +1,19 @@
+@REM Encrypt yara rule to avoid unncessary security alerts.
+
+python utils\arc4.py -f .\rsrc\ioc\rule.yara
+
+@REM Initialize build derectory
+
+mkdir build
+cd .\build
+echo y | del *
+
+@REM Build YAMA Scanner
+
+cmake -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang.exe" ^
+      -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang++.exe" ^
+      -DCMAKE_RC_COMPILER="C:/Program Files/LLVM/bin/llvm-rc.exe" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -G Ninja ..
+ninja 
+cd ..\
